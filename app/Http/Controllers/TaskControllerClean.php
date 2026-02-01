21406d4f56760758fcd48143bcdff2183eb0c7d0
abdelilah abdi <abdelilahabdi6633@gmail.com>
2026-02-01 01:25:30 +0100

    edit en task controllers

--- app/Http/Controllers/TaskControllerClean.php
+++ app/Http/Controllers/TaskControllerClean.php
@@ -4,3 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Models\Task;
 use Illuminate\Http\Request;
+
+
+class TaskController extends Controller
+{
+    /**
+     * Afficher la liste des tâches avec filtres
+     */
+    public function index(Request $request)
+    {
+        $query = auth()->user()->tasks();
+        
+        // Application des filtres
+        $this->applyFilters($query, $request);
+        $this->applySorting($query, $request);
+        
+        $tasks = $query->paginate(10);
+        
+        return view('tasks.professional-index', compact('tasks'));
+    }
